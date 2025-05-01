0f68ab0120544b355277a976457031af7d2b06da
MateoPalacios004 <mateo.palacios.donoso@udla.edu.ec>
2025-05-01 15:29:34 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        Sala[] salas = { new Sala(1), new Sala(2), new Sala(3) };
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int opcion;
+        do {
+            System.out.println(" Cinema Paradiso");
+            System.out.println("1. Mostrar disponibilidad");
+            System.out.println("2. Comprar asientos");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    for (Sala sala : salas) {
+                        sala.mostrarDisponibilidad();
+                    }
+                    break;
+                case 2:
+                    System.out.print("Ingrese número de sala: ");
+                    int numSala = sc.nextInt();
+                    if (numSala < 1 || numSala > 3) {
+                        System.out.println("Sala inválida.");
+                        break;
+                    }
+
+                    System.out.print("Seleccione el numero de asientos que dese comprar: ");
+                    int cantidad = sc.nextInt();
+                    salas[numSala - 1].comprarAsientos(cantidad);
+                    break;
+                case 3:
+                    System.out.println("Gracias por usar el sistema.");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        } while (opcion != 3);
+
+        sc.close();
     }
 }
--- src/Sala.java
+++ src/Sala.java
@@ -1,4 +1,88 @@
-package PACKAGE_NAME;
+import java.util.PriorityQueue;
 
-public class Sala {
+public class Sala implements Comparable<Sala> {
+    private int id;
+    private final int capacidad = 25;
+    private int asientosv;
+    private int asientosd;
+    private PriorityQueue<Integer> asientosDisponibles;
+
+
+    public Sala(int id) {
+        this.id = id;
+        this.asientosv = 0;
+        this.asientosd = capacidad;
+        this.asientosDisponibles = new PriorityQueue<>();
+        for (int i = 1; i <= capacidad; i++) {
+            asientosDisponibles.add(i);
+        }
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public int getAsientosVendidos() {
+        return asientosv;
+    }
+
+    public void setAsientosVendidos(int asientosv) {
+        this.asientosv = asientosv;
+    }
+
+    public int getAsientosDisponibles() {
+        return asientosd;
+    }
+
+    public void setAsientosDisponibles(int asientosd) {
+        this.asientosd = asientosd;
+    }
+
+
+    public boolean comprarAsientos(int cantidad) {
+        if (cantidad < 1 || cantidad > 4) {
+            System.out.println("No se puede comprar más de 4 asientos");
+            return false;
+        }
+
+        if (asientosDisponibles.size() < cantidad) {
+            System.out.println("Insuficiencia de asientos disponibles.");
+            return false;
+        }
+
+        System.out.print("Asientos asignados: ");
+        for (int i = 0; i < cantidad; i++) {
+            int asiento = asientosDisponibles.poll();
+            System.out.print(asiento + " ");
+            asientosv++;
+            asientosd--;
+        }
+        System.out.println();
+        return true;
+    }
+
+
+    public void mostrarDisponibilidad() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Sala numero:  " + id + ": " + "Los asientos disponibles son: "+ asientosd + "   :   " + "Los asientos vendidos son: " + asientosv;
+    }
+
+    @Override
+    public int compareTo(Sala otraSala) {
+        return Integer.compare(this.asientosd, otraSala.asientosd);
+    }
 }
+
